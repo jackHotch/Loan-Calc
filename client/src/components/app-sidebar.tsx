1e6008329e6d53f708c7e76c0da1592979f13249
jackHotch <jhotchkiss2021@gmail.com>
2026-01-30 00:21:26 -0500

    added shadcn sidebar

---
+++ client/src/components/app-sidebar.tsx
@@ -0,0 +1,72 @@
+'use client'
+
+import * as React from 'react'
+import { DollarSign, Calculator, ChartLine, Layers, ArrowLeftRight } from 'lucide-react'
+
+import { NavList } from '@/components/nav-list'
+import { NavUser } from '@/components/nav-user'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+import Link from 'next/link'
+
+const data = {
+  user: {
+    name: 'shadcn',
+    email: 'm@example.com',
+    avatar: '/avatars/shadcn.jpg',
+  },
+  navList: [
+    {
+      title: 'Dashboard',
+      url: '/dashboard',
+      icon: ChartLine,
+    },
+    {
+      title: 'Loans',
+      url: '/loans',
+      icon: DollarSign,
+    },
+    {
+      title: 'Simulations',
+      url: '/simulations',
+      icon: Layers,
+    },
+    {
+      title: 'Compare',
+      url: '/compare',
+      icon: ArrowLeftRight,
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible='offcanvas' {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild className='data-[slot=sidebar-menu-button]:p-1.5! hover:bg-transparent'>
+              <Link href='/dashboard'>
+                <Calculator className='size-5!' color='var(--primary)' />
+                <span className='text-base font-semibold'>Loan Calculator</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavList items={data.navList} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
---
+++ client/src/components/nav-list.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+import { useRouter } from 'next/navigation'
+
+export function NavList({
+  items,
+}: {
+  items: {
+    title: string
+    url: string
+    icon?: any
+  }[]
+}) {
+  const router = useRouter()
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent className='flex flex-col gap-2'>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton tooltip={item.title} onClick={() => router.push(item.url)}>
+                {item.icon && <item.icon />}
+                <span>{item.title}</span>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  )
+}
