723aa347fe033dd9bceab8cb64def9d71b3f103a
jackHotch <jhotchkiss2021@gmail.com>
2026-01-29 22:24:00 -0500

    created basic hero section

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <div className='p-4'>hi</div>
+  return (
+    <div className='flex flex-col justify-center items-center p-8'>
+      <div className='flex flex-col items-center gap-4 w-fit'>
+        <h1 className='text-6xl font-bold'>Loan Calculator</h1>
+        <p className='text-zinc-500'>A tool to help you manage your debt and spend less money</p>
+        <button className=''>Start Saving</button>
+      </div>
+    </div>
+  )
 }
--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -7,19 +7,22 @@ export const Header = () => {
       <header className='flex justify-between items-center p-6 gap-4 h-16'>
         <div className='flex items-center gap-2'>
           <Calculator className='text-primary-500' size={32} />
-          <h1 className='text-3xl text-center font-bold'>Student Loan Calculator</h1>
         </div>
-        <SignedOut>
-          <SignInButton />
-          <SignUpButton>
-            <button className='bg-primary-500 text-white rounded font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer'>
-              Sign Up
-            </button>
-          </SignUpButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
+        <div className='flex gap-8'>
+          <SignedOut>
+            <SignInButton>
+              <button className='cursor-pointer bg-transparent dark:text-white'>Sign in</button>
+            </SignInButton>
+            <SignUpButton>
+              <button className='bg-primary-500 text-white rounded font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer'>
+                Sign Up
+              </button>
+            </SignUpButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
       </header>
       <hr className='text-primary-500' />
     </>
