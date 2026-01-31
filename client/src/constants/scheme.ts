e18ad819e3353cc8696a636f6501c89e8bcad733
jackHotch <jhotchkiss2021@gmail.com>
2026-01-31 17:36:50 -0500

    updated edit screen and created new loan screen

---
+++ client/src/constants/scheme.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod'
+
+export const loanTableSchema = z.object({
+  name: z.string(),
+  current_balance: z.string(),
+  interest_rate: z.string(),
+  lender: z.string(),
+  starting_principal: z.string(),
+  remaining_principal: z.string(),
+  accrued_interest: z.string(),
+  payoff_date: z.string(),
+})
