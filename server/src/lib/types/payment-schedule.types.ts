ddcf77aba6eaef7a97b2b656d347a9e1a73b8811
jackHotch <jhotchkiss2021@gmail.com>
2026-02-08 22:24:02 -0500

    added payment schedule to new loan. interest still not working

--- server/src/lib/types/payment-schedule.types.ts
+++ server/src/lib/types/payment-schedule.types.ts
@@ -1,9 +1,7 @@
 export interface PaymentScheduleInput {
   current_principal: number;
-  accrued_interest: number;
   interest_rate: number;
   start_date: Date;
-  payoff_date: Date;
   minimum_payment: number;
   extra_payment: number | null;
   extra_payment_start_date: Date | null;
@@ -14,14 +12,12 @@ export interface PaymentScheduleEntry {
   payment_date: Date;
   principal_paid: number;
   interest_paid: number;
-  total_extra_payment: number;
+  extra_payment: number;
   remaining_principal: number;
-  remaining_interest: number;
 }
 
 export interface CalculateScheduleOptions {
   startFromPaymentNumber?: number;
   startingPrincipal?: number;
-  startingInterest?: number;
   startDate?: Date | string;
 }
