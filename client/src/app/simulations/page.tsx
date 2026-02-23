0ef21766fc6763607abccffd1fc28a495e4dfa5a
jackHotch <jhotchkiss2021@gmail.com>
2026-02-22 22:08:10 -0500

    created sim page header

--- client/src/app/simulations/page.tsx
+++ client/src/app/simulations/page.tsx
@@ -1,3 +1,24 @@
+import { ActiveSimulation } from '@/components/active-simulation'
+import { Button } from '@/components/ui/button'
+import { Plus } from 'lucide-react'
+
 export default function Simulations() {
-  return <div>simulations</div>
+  return (
+    <div>
+      <header className='w-full flex justify-between items-end px-8 py-16'>
+        <div>
+          <h1 className='font-display text-6xl font-light'>Simulations</h1>
+          <p className='text-zinc-500 text-xs'>2 saved</p>
+        </div>
+
+        <div className='w-max'>
+          <Button>
+            <Plus />
+            <span className='hidden md:inline text-xs tracking-widest'>NEW SIMULATION</span>
+          </Button>
+        </div>
+      </header>
+      <ActiveSimulation />
+    </div>
+  )
 }
